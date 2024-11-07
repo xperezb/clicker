9d57cf68fc0a6c99721a521f18499cf01cb81849
xperez <xperezb@hotmail.com>
2024-11-07 22:45:09 +0100

    added upgrade.icon

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Upgrade } from '../interfaces/upgrade';
+import { FarmComponent } from '../components/empire-view/components/farm.component';
 
 @Injectable({
   providedIn: 'root',
@@ -15,23 +16,24 @@ export class GameService {
   private clickUpgradeCount = 0;
 
   upgrades: Upgrade[] = [
-    { id: 1, name: 'Plant Marihuana', cost: 10, increase: 1, count: 0, requiredPoints: 0 },
-    { id: 2, name: 'Sling Cocaine', cost: 50, increase: 5, count: 0, requiredPoints: 100 },
-    { id: 3, name: 'Sell Heroin', cost: 100, increase: 10, count: 0, requiredPoints: 200 },
-    { id: 4, name: 'Produce Meth', cost: 500, increase: 50, count: 0, requiredPoints: 300 },
-    { id: 5, name: 'Traffic Humans', cost: 1000, increase: 100, count: 0, requiredPoints: 400 },
+    { id: 1, name: 'Plant Marihuana', cost: 10, costIncrease: 0, pointsIncrease: 1, count: 0, requiredPoints: 0, icon: FarmComponent },
+    { id: 2, name: 'Sling Cocaine', cost: 50, costIncrease: 0, pointsIncrease: 5, count: 0, requiredPoints: 100 },
+    { id: 3, name: 'Sell Heroin', cost: 100, costIncrease: 0, pointsIncrease: 10, count: 0, requiredPoints: 200 },
+    { id: 4, name: 'Produce Meth', cost: 500, costIncrease: 0, pointsIncrease: 50, count: 0, requiredPoints: 300 },
+    { id: 5, name: 'Traffic Humans', cost: 1000, costIncrease: 0, pointsIncrease: 100, count: 0, requiredPoints: 400 },
   ];
-
+  
   clickUpgrades: Upgrade[] = [
-    { id: 1, name: 'Bike', cost: 10, increase: 1, count: 0, requiredPoints: 0 },
-    { id: 2, name: 'MotorBike', cost: 100, increase: 5, count: 0, requiredPoints: 100 },
-    { id: 3, name: 'Ahmed', cost: 1000, increase: 10, count: 0, requiredPoints: 200 },
-    { id: 4, name: 'M13 Gang Member', cost: 10000, increase: 50, count: 0, requiredPoints: 300 },
-    { id: 5, name: '18ST Gang Member', cost: 10000, increase: 50, count: 0, requiredPoints: 300 },
-    { id: 6, name: 'Pick-up Truck', cost: 50000, increase: 100, count: 0, requiredPoints: 400 },
-    { id: 7, name: 'Local Police Officer', cost: 100000, increase: 1500, count: 0, requiredPoints: 1000 }, 
-    { id: 8, name: 'FBI Agent', cost: 1000000, increase: 2000, count: 0, requiredPoints: 2000 },
-    { id: 9, name: 'CIA Agent', cost: 5000000, increase: 2000, count: 0, requiredPoints: 2000 }
+    { id: 0, name: 'Magic button', cost: 0, costIncrease: 0, pointsIncrease: 1000000, count: 0, requiredPoints: 0, icon: FarmComponent },
+    { id: 1, name: 'Bike', cost: 10, costIncrease: 0, pointsIncrease: 1, count: 0, requiredPoints: 0 },
+    { id: 2, name: 'MotorBike', cost: 100, costIncrease: 0, pointsIncrease: 5, count: 0, requiredPoints: 100 },
+    { id: 3, name: 'Ahmed', cost: 1000, costIncrease: 0, pointsIncrease: 10, count: 0, requiredPoints: 200 },
+    { id: 4, name: 'M13 Gang Member', cost: 10000, costIncrease: 0, pointsIncrease: 50, count: 0, requiredPoints: 300 },
+    { id: 5, name: '18ST Gang Member', cost: 10000, costIncrease: 0, pointsIncrease: 50, count: 0, requiredPoints: 300 },
+    { id: 6, name: 'Pick-up Truck', cost: 50000, costIncrease: 0, pointsIncrease: 100, count: 0, requiredPoints: 400 },
+    { id: 7, name: 'Local Police Officer', cost: 100000, costIncrease: 0, pointsIncrease: 1500, count: 0, requiredPoints: 1000 }, 
+    { id: 8, name: 'FBI Agent', cost: 1000000, costIncrease: 0, pointsIncrease: 2000, count: 0, requiredPoints: 2000 },
+    { id: 9, name: 'CIA Agent', cost: 5000000, costIncrease: 0, pointsIncrease: 2000, count: 0, requiredPoints: 2000 }
   ];
 
   points$ = new BehaviorSubject<number>(this.points);
@@ -86,7 +88,7 @@ export class GameService {
     const upgrade = this.upgrades.find(u => u.id === upgradeId);
     if (upgrade && this.points >= upgrade.cost) {
       this.points -= upgrade.cost;
-      this.pointsPerSecond += upgrade.increase;
+      this.pointsPerSecond += upgrade.pointsIncrease;
       upgrade.count += 1;
       upgrade.cost = Math.floor(upgrade.cost + 5);
 
@@ -102,7 +104,7 @@ export class GameService {
     const upgrade = this.clickUpgrades.find(u => u.id === upgradeId);
     if (upgrade && this.points >= upgrade.cost) {
       this.points -= upgrade.cost;
-      this.pointsPerClick += upgrade.increase;
+      this.pointsPerClick += upgrade.pointsIncrease;
       upgrade.count += 1;
       upgrade.cost = Math.floor(upgrade.cost + 5);
 
